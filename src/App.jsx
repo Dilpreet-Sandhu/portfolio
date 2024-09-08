a84ba017314fa9de69130ecdf6e15b469a5543a9
Dilpreet <155355461+Dilpreet-Sandhu@users.noreply.github.com>
2024-09-08 13:01:46 +0530

    project initiated

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useRef, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './sections/Navbar'
+
+
+
+
+function App() {
+ 
+
+  return (
+   <main className='max-w-7xl mx-auto'>
+      <Navbar/>
+    </main>
+  )
+}
+
+export default App
